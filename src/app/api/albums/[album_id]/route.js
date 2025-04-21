95dd13d2fe9d053bd6308204dc01b37246441ebd
Jourdann Fraser <jourdann.fraser@gmail.com>
2025-04-21 15:22:22 -0400

    Added route for get of individual album

--- src/app/api/albums/[album_id]/route.js
+++ src/app/api/albums/[album_id]/route.js
@@ -1,6 +1,20 @@
 import pool from "@/lib/db";
 
+export async function GET(request, {params}) {
+    const {album_id} = await params;
 
+    try {
+        const res = await pool.query(`SELECT * from albums WHERE album_id = $1`, [album_id]);
+
+        return Response.json(res.rows, {status: 200});
+        
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+
+    
+}
 
 export async function DELETE(request, {params}) {
     const {album_id} = await params;
