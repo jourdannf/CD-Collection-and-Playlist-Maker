14311fba0c7fa8d08685d5e0f23b71508e567e3c
Jourdann Fraser <jourdann.fraser@gmail.com>
2025-08-19 17:01:30 -0400

    Changed implementation of stacked cds

--- src/app/playlist-maker/(components)/PlaylistCreatedPage.jsx
+++ src/app/playlist-maker/(components)/PlaylistCreatedPage.jsx
@@ -6,13 +6,8 @@ export default function PlaylistCreatedPage ({insideBoombox}) {
 
     return (
         <div className="text-center content-center h-screen">
-            <h4 className="mb-8">GRAB THESE CDS AND START MIXING</h4>
-            <div className="space-y-2 mb-13">
-            {insideBoombox.map((track) => {
-                console.log(track)
-                return <StackedCDs key={track.track_id} track={track}/>
-            })}
-            </div>
+            <h4 className="mb-8">GRAB THESE CDS AND START MIXING</h4>                        
+            <StackedCDs insideBoombox={insideBoombox} />
             <Button variant="secondary" className="block mb-2.5 mx-auto">Make Another Mix</Button>
             <Button variant="secondary" className="block mx-auto">Add To My Collection</Button>
         </div>
--- src/app/playlist-maker/(components)/StackedCDs.jsx
+++ src/app/playlist-maker/(components)/StackedCDs.jsx
@@ -1,15 +1,15 @@
-import gsap from "gsap";
+import CDSpine from "./CDSpine";
 
-export default function StackedCDs({track}) {
-    const trackDate = new Date(track.release_date);
+export default function StackedCDs({insideBoombox}) {
+    // const trackDate = new Date(track.release_date);
     
     return (
-        <>
-            <div className="border-x-8 border-y-6 grid grid-cols-3 px-6 w-[708px] mx-auto bg-push-play-purple-600/50 border-push-play-charcoal-200 text-push-play-charcoal-900 cd">
-                <p className="text-left">{track.artist_name}</p>
-                <p className="text-center">{track.title}</p>
-                <p className="text-right">{trackDate.getFullYear()}</p>
-            </div>
-        </>
+        <div className="space-y-2 mb-13">
+        {
+            insideBoombox.map((track) => {
+                return <CDSpine track={track} />
+            })
+        }
+        </div>
     )
 }
